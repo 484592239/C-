20df304bc10841d30d21ffec768ed5058110e9c6
Asuka <92673831+484592239@users.noreply.github.com>
2021-10-17 19:56:49 +0800

    Add files via upload

---
+++ 长方体体积.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define HEG 10
+int calculate(int Long,int Width);
+int main()
+{
+	int m_Long;
+	int m_Width;
+	int result;
+	
+	printf("长方形的高度为：%d\n",HEG);
+	
+	printf("请输入长度\n");
+	scanf("%d",&m_Long);
+	
+	printf("请输入宽度\n");
+	scanf("%d",&m_Width);
+	
+	result=calculate(m_Long,m_Width);
+	printf("长方体的体积是");
+	printf("%d\n",result);
+	return 0;
+}
+ 
+ int calculate(int Long,int Width)
+ {
+ 	int result=Long*Width*HEG;
+ 	return result;
+ }
